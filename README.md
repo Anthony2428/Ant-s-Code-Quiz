058addbc93778efe169739f68c093a8a0446c8bf
Ant <alopez0218.al@gmail.com>
2021-03-29 18:59:48 -0700

    (Deployment)

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Ant-s-Code-Quiz
+-----Deployment-----</br>
+Presented at the start up screen, is the date and an explanation of how to play the quiz.</br>
+Upon clicking the Start Button:</br>
+1.) The user will go through a series of questions</br>
+2.) With 20 seconds on the clock each question will award points, while each incorrect answer will deduct 3 seconds on the clock</br></br>
+
+Upon hitting the time limit or answering every question correctly, the user is presented a leaderboard with the option of submitting their initials or playing again.</br>
+1.) Initials are restricted to 3 letters, not allowing the user to keydown non-alphabetical characters</br>
+2.) Leaderboard is not in order, will only submit one initial per attempt.</br>
+3.) Score is the time left when you answered correctly, added into a total.</br></br>
+
+![Screenshot of Web App:](./assets/Code-Quiz-Screenshot.png)
+
+GITHUB URL: https://github.com/Anthony2428/Ant-s-Code-Quiz/</br></br>
+
+# User Story
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+
+# 04 Web-APIs: Code Quiz
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,141 +1,198 @@
+//Startup methods
 var today = moment();
 $("#date").text(today.format("MMMM Do, YYYY"));
 $("#timerspace").hide();
 $("#scoreboard").hide();
+$("#leaderboard").hide();
+
+
+//Linking the html 
 const start = document.querySelector("#Start");
+const playAgain = document.querySelector("#playAgain");
 const choiceA = document.querySelector("#A");
 const choiceB = document.querySelector("#B");
 const choiceC = document.querySelector("#C");
 const choiceD = document.querySelector("#D");
 let next = false;
 let points = 0;
-Start.addEventListener("click", StartQuiz);
-const timer = document.getElementById("timer");
 const scoreboard = document.getElementById("scoreboard");
-var totalTime = 60;
+let totalTime = 2000;
+const submit = document.querySelector("#submit");
+const initials = $('input[id="initials"]');
+const rankings = document.querySelector("#rankings");
+const timer = document.getElementById("timer")
 
-
+//Declares each Question
 const Question1 = {
     name: 'Question 1',
-    answer: ["Apple", "Banana", "Oranges", "Grapes"],
-    question: 'What fruit do you want?',
-    correct: "A"
+    answer: ["At the end of the body tag", "In the head tag", "In the beginning of the body tag", "It doesn't go in the html"],
+    question: 'Where should you reference the js file in your html?',
+    correct: "A",
 };
-
 const Question2 = {
     name: 'Question 2',
     answer: ["Baseball", "Hockey", "Basketball", "Football"],
     question: 'What sport do you want?',
-    correct: "D"
+    correct: "D",
 };
-
 const Question3 = {
     name: 'Question 3',
     answer: ["Apple", "Banana", "Oranges", "Grapes"],
     question: 'What fruit do you want.',
-    correct: "A"
+    correct: "A",
 };
-
 const Question4 = {
     name: 'Question 4',
     answer: ["Apple", "Banana", "Oranges", "Grapes"],
     question: 'What fruit do you want.',
-    correct: "A"
+    correct: "A",
 };
-
 const Question5 = {
     name: 'Question 5',
     answer: ["Apple", "Banana", "Oranges", "Grapes"],
     question: 'What fruit do you want.',
-    correct: "C"
+    correct: "C",
 };
-
 const Question6 = {
     name: 'Question 6',
     answer: ["Apple", "Banana", "Oranges", "Grapes"],
     question: 'What fruit do you want.',
-    correct: "D"
+    correct: "D",
 };
-
 const Question7 = {
     name: 'Question 7',
     answer: ["Apple", "Banana", "Oranges", "Grapes"],
     question: 'What fruit do you want.',
-    correct: "C"
+    correct: "C",
+};
+const Question8 = {
+    name: 'Question 8',
+    answer: ["int num = 1", "num = 1", "const num = 1", "1 = num"],
+    question: 'Which is the correct declaration of a variable that equals 1?',
+    correct: "C",
+};
+const Question9 = {
+    name: 'Question 9',
+    answer: ["Apple", "Banana", "Oranges", "Grapes"],
+    question: '',
+    correct: "C",
+};
+const Question10 = {
+    name: 'Question 10',
+    answer: ["Apple", "Banana", "Oranges", "Grapes"],
+    question: 'What fruit do you want.',
+    correct: "C",
 };
 
-async function SetProblem(Question) {
+//Places all questions into one bucket
+const Questions = [Question1, Question2, Question3, Question4, Question5, Question6, Question7, Question8, Question9, Question10]
+
+//Counter
+let i = 0;
+
+//Sets the Question
+const SetProblem = function(cQuestion) {
+    scoreboard.textContent = points;
+    let Question = cQuestion
     $("#question").text(Question.name);
     $("#problem").text(Question.question);
     $("#A").text(Question.answer[0]);
     $("#B").text(Question.answer[1]);
     $("#C").text(Question.answer[2]);
     $("#D").text(Question.answer[3]);
-    let correctAnswer = Question.correct;
 
-    let result = await CheckAnswer(correctAnswer);
-    if (result === true) {
-        points += totalTime
-    }
-    return new Promise(resolve => {
-        resolve(console.log("Question Answered")); 
-    });
+    choiceA.addEventListener("click", checkAnswer);
+    choiceB.addEventListener("click", checkAnswer);
+    choiceC.addEventListener("click", checkAnswer);
+    choiceD.addEventListener("click", checkAnswer);
+
+    return;
 }
-function CheckAnswer(correctAnswer) {
-    return new Promise(resolve => {
-    $('#A').click(() => { if (correctAnswer === "A") {
-        resolve(true) 
-    }else {
-        totalTime -= 3
-        timer.textContent = totalTime + " seconds left to finish the quiz.";
-    }});
-    $('#B').click(() => { if (correctAnswer === "B") {
-        resolve(true) 
-    }else {
-        totalTime -= 3
-        timer.textContent = totalTime + " seconds left to finish the quiz.";
-    }});
-    $('#C').click(() => { if (correctAnswer === "C") {
-        resolve(true) 
-    }else {
-        totalTime -= 3
-        timer.textContent = totalTime + " seconds left to finish the quiz.";
-    }});
-    $('#D').click(() => { if (correctAnswer === "D") {
-        resolve(true) 
-    }else {
-        totalTime -= 3
-        timer.textContent = totalTime + " seconds left to finish the quiz.";
-    }});
-    });
-}
-
-
-function setTime() {
+//Checks to see if user inputted the correct or the wrong answer
+const checkAnswer = function(event) {
+    let element = event.target;
     
-    var timerInterval = setInterval(function() {
-        timer.textContent = totalTime + " seconds left to finish the quiz.";
-        totalTime--;
-  
-        if(totalTime <= 0) {
-            clearInterval(timerInterval);
-            timer.textContent = "Time's Up!";
+    if (element.value === Questions[i].correct) {
+        points += totalTime;
+        console.log("Next Question");
+        i++;
+        if (i === 10) {
             endGame();
+            return;
         }
-    }, 1000);
-  }
-async function StartQuiz(event) {
-    const Questions = [Question1, Question2, Question3, Question4, Question5, Question6, Question7]
+        SetProblem(Questions[i])
+    } else {
+        totalTime -= 300;
+        console.log("-3 seconds on the clock");
+    }
+}
+//Starts the timer, and upon reaching 0 will end the game
+const setTime = function () {
+    let time = setInterval(() => {
+        if(totalTime <= 0) {
+            clearInterval(time);
+            endGame();
+            return;
+        }
+        totalTime--;
+        timer.innerHTML = totalTime/100 + " seconds left to finish the quiz.";
+        return;
+    }, 10)
+}
+//Starts the Quiz
+const StartQuiz = function(event) {
+    points = 0;
+    i = 0;
+    event.preventDefault();
+    totalTime = 2000
+    $("#Start").hide();
+    $("#enterInitials").show();
+    $("#leaderboard").hide();
     $("#title").hide();
+    $("#quiz").show();
     $("#timerspace").show();
     setTime();
     $("#scoreboard").show();
-    for (let i = 0; i < Questions.length; i++) {
-        await SetProblem(Questions[i]);
-        scoreboard.textContent = points;
-    }
-    endGame();
+    SetProblem(Questions[i]);
+    return;
 }
-function endGame() {
+//Ends the game (Hide Quiz & Display Leaderboard)
+const endGame = function() {
+    totalTime = 0;
     $("#quiz").hide();
+    $("#leaderboard").show();
+    timer.innerHTML = "Time's Up!";
+    scoreboard.textContent = "You're score is " + points;
+    submit.addEventListener("click", submitInitial);
+    return;
+}
+
+//Button Handlers for Starting the Quiz, Playing Again
+start.addEventListener("click", StartQuiz);
+playAgain.addEventListener("click", StartQuiz);
+submit.addEventListener("click", submitInitial);
+
+//Restrict all keys but the alphabet keys from being pressed
+const restrictTheseKeys = function(event) {
+    var e = window.event; // gets the key thats pressed
+    var key = e.keyCode; // gets the key's code that was pressed
+
+    if ((key < 65 || key > 90) && (key !== 8)) { //if it is a letter, prevent default
+        e.preventDefault(); //normal browsers
+    }
+}
+//Submitting Initials Function
+function submitInitial(event) {
+    event.preventDefault();
+    if (initials.val().length >= 2 && isNaN(initials.val())) {
+        let p = document.createElement('li');
+        p.append(document.createTextNode(initials.val().toUpperCase() + " - " + points + " PTS"));
+        rankings.append(p);
+        submit.removeEventListener("click", submitInitial);
+        $("#enterInitials").hide();
+    } else {
+        alert("Initials must be 2-3 letters")
+    }
+    initials.val("")
+    return
 }
